68cd339c70d243df39e1ad4b03cf509f4da17d9f
Mihail Fox <foxmihail83@gmail.com>
2020-05-24 23:00:09 +0300

    solve 'Strain' exercise without using Linq.

--- strain/Strain.cs
+++ strain/Strain.cs
@@ -5,11 +5,29 @@ public static class Strain
 {
     public static IEnumerable<T> Keep<T>(this IEnumerable<T> collection, Func<T, bool> predicate)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var output = new List<T>();
+        foreach (var item in collection)
+        {
+            if (predicate(item))
+            {
+                output.Add(item);
+            }
+        }
+
+        return output;
     }
 
     public static IEnumerable<T> Discard<T>(this IEnumerable<T> collection, Func<T, bool> predicate)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var output = new List<T>();
+        foreach (var item in collection)
+        {
+            if (!predicate(item))
+            {
+                output.Add(item);
+            }
+        }
+
+        return output;
     }
 }
