c8f6a2262755751e28b2493e1fa976360537c35c
Mihail Fox <foxmihail83@gmail.com>
2020-05-24 21:16:46 +0300

    solve 'Raindrops'

--- raindrops/Raindrops.cs
+++ raindrops/Raindrops.cs
@@ -1,9 +1,42 @@
-using System;
+using System.Collections.Generic;
 
 public static class Raindrops
 {
     public static string Convert(int number)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var raindrops = new Dictionary<int, string>
+        {
+            { 3, "Pling" },
+            { 5, "Plang" },
+            { 7, "Plong" }
+        };
+
+        var factors = GetFactors(number);
+        var output = string.Empty;
+
+        foreach (var factor in factors)
+        {
+            if (raindrops.ContainsKey(factor))
+            {
+                output += raindrops[factor];
+            }
+        }
+
+        return string.IsNullOrEmpty(output) ? number.ToString() : output;
+    }
+
+    private static List<int> GetFactors(int number)
+    {
+        var factors = new List<int>();
+
+        for (int i = 1; i <= number; i++)
+        {
+            if (number % i == 0)
+            {
+                factors.Add(i);
+            }
+        }
+
+        return factors;
     }
 }
