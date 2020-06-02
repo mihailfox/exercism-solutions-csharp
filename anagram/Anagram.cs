dd0fce2d9c9a88696969e7d4377d5682a3f32827
Mihail Fox <foxmihail83@gmail.com>
2020-06-02 04:26:09 +0300

    solve 'Anagram' exercise.

--- anagram/Anagram.cs
+++ anagram/Anagram.cs
@@ -1,14 +1,41 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 public class Anagram
 {
+    private readonly string BaseWord;
+
     public Anagram(string baseWord)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        BaseWord = baseWord;
     }
 
     public string[] FindAnagrams(string[] potentialMatches)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var anagrams = new List<string>();
+
+        foreach (var word in potentialMatches)
+        {
+            if (IsAnagram(word, BaseWord))
+            {
+                anagrams.Add(word);
+            }
+        }
+
+        return anagrams.ToArray();
+    }
+
+    private bool IsAnagram(string inputOne, string inputTwo)
+    {
+        if (string.Equals(inputOne, inputTwo, StringComparison.OrdinalIgnoreCase))
+        {
+            return false;
+        }
+
+        var sortedOne = string.Concat(inputOne.ToLower().OrderBy(c => c));
+        var sortedTwo = string.Concat(inputTwo.ToLower().OrderBy(c => c));
+
+        return sortedOne == sortedTwo;
     }
 }
