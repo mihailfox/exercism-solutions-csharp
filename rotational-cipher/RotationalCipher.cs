c6d9ee8dd2cf595b7263597211fee9624a4648b5
Mihail Fox <foxmihail83@gmail.com>
2020-07-10 17:28:34 +0300

    add solution for RotationalCipher.

--- rotational-cipher/RotationalCipher.cs
+++ rotational-cipher/RotationalCipher.cs
@@ -1,9 +1,47 @@
-using System;
+using System.Text;
 
 public static class RotationalCipher
 {
     public static string Rotate(string text, int shiftKey)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var output = new StringBuilder();
+        foreach (var inputChar in text)
+        {
+            output.Append(Transform(inputChar, shiftKey));
+        }
+        return output.ToString();
+    }
+
+    private static char Transform(char inputChar, int shiftKey)
+    {
+        if (!char.IsLetter(inputChar))
+        {
+            return inputChar;
+        }
+
+        bool isUpper = char.IsUpper(inputChar);
+        int firstLetterCode = isUpper ? 'A' : 'a';
+        int lastLetterCode = isUpper ? 'Z' : 'z';
+
+        int outputCharCode = inputChar + shiftKey;
+
+        var output = (char)(outputCharCode <= lastLetterCode
+                    ? outputCharCode
+                    : Mod(outputCharCode, lastLetterCode) + firstLetterCode - 1);
+
+        return output;
+    }
+
+    private static int Mod(int dividend, int divisor)
+    {
+        if (divisor == 0)
+        {
+            return dividend;
+        }
+
+        int remainder = dividend % divisor;
+        return remainder < 0
+            ? remainder + divisor
+            : remainder;
     }
 }
